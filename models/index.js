7a6a32f33a8742cb28ad9bd53fee6604df67305c
ZZOMAN <reumia@gmail.com>
2017-09-21 18:31:32 +0900

    Comics 생성 테스트 코드 작성 및 Comics API 기본 구문 작성

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ module.exports = function (db, cb) {
     const LikesComic = db.models['likes_comic']
     const LikesCut = db.models['likes_cut']
 
-    Comics.hasOne('owner', Users)
+    Comics.hasOne('owner', Users, {reverse: 'users'})
     Cuts.hasOne('owner', Users)
     Cuts.hasOne('comic', Comics)
     LikesUser.hasOne('target', Users)
@@ -27,6 +27,7 @@ module.exports = function (db, cb) {
     db.sync(err => {
         if (err) throw err
 
+        // 유저 생성
         // Users.create({
         //     status: 'OPENED',
         //     password: 'asdqwe',
@@ -37,6 +38,24 @@ module.exports = function (db, cb) {
         // }, err => {
         //     if (err) throw err
         //     console.log('User Added.')
+        // })
+
+        // 코믹 생성
+        // Comics.create({
+        //     status: 'CLOSED',
+        //     title: '흑흑맨의 일기',
+        //     descriptions: '항상 우는 흑흑맨의 일기입니다. 자칫 너무 우울할 수 있으니 조심해 X꺄.',
+        // }, (err, comics) => {
+        //     if (err) throw err
+        //     Users.getAsync(2)
+        //         .then(user => {
+        //             comics.setOwner(user, err => {
+        //                 if (err) throw err
+        //                 console.log('Comic Added.')
+        //             })
+        //         }).error(err => {
+        //             if (err) throw err
+        //         })
         // })
     })
 
--- routes/comics.js
+++ routes/comics.js
@@ -3,14 +3,29 @@ import express from 'express'
 const router = express.Router()
 require('./middlewares')(router)
 
-// define the home page route
-router.get('/', function(req, res) {
-    res.send('Birds home page')
+/* GET ALL COMICS */
+router.get('/', (req, res) => {
+    const Comics = req.db.models.comics
+
+    console.log(`GET All Opened Comics.`)
+
+    Comics.all({}, (err, comics) => {
+        if (err) res.status(500).json({err: err})
+        res.status(200).json(comics)
+    })
 })
 
-// define the about route
-router.get('/about', function(req, res) {
-    res.send('About birds')
+/* GET USER BY ID */
+router.get('/:id', (req, res) => {
+    const Comics = req.db.models.comics
+    const comicId = req.params.id
+
+    console.log(`GET Comic ${comicId}.`)
+
+    Comics.get(comicId, (err, comic) => {
+        if (err) res.status(500).json({err: err})
+        res.status(200).json(comic)
+    })
 })
 
 module.exports = router
