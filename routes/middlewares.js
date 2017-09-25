a4ba5706be90bf6745f7f57b9b8181ba8fff61f6
ZZOMAN <reumia@gmail.com>
2017-09-25 18:43:43 +0900

    TODO 추가

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -4,6 +4,7 @@ module.exports = (router) => {
         next()
     })
     router.use(function isAuthorized(req, res, next) {
+        // TODO : 로그인 여부 인증처리
         console.log(Boolean(req.session.email))
         if (Boolean(req.session.email) === false) res.status(401)
         next()
