7983f1ed554c26da9760233ae305f5fc57d0eefa
ZZOMAN <reumia@gmail.com>
2017-09-26 12:03:30 +0900

    Authorization Middleware 임시제거

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = (router) => {
-    router.use(function timeLog(req, res, next) {
-        console.log('Time: ', Date.now())
-        next()
-    })
-    router.use(function checkAuthorization(req, res, next) {
-        const isAuthorized = Boolean(req.session.email) === true
+const timeLog = (req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+}
+const checkAuthorization = (req, res, next) => {
+    const isAuthorized = Boolean(req.session.email) === true
+    const ignores = [
+        'users',
+        'user/',
+        'comics',
+        'comic/'
+    ]
 
-        if (isAuthorized) next()
-        else res.status(401).send()
-    })
+    if (isAuthorized) next()
+    else res.status(401).send()
+}
+
+module.exports = (router) => {
+    router.use(timeLog)
 }
